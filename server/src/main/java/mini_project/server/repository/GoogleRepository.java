fd914c59bb8e90b9b167ccdaa40af96f2df87d11
LydiaThamy <lydiathamy@gmail.com>
2023-09-08 15:26:50 +0800

    clean code

--- server/src/main/java/mini_project/server/repository/GoogleRepository.java
+++ server/src/main/java/mini_project/server/repository/GoogleRepository.java
@@ -1,12 +1,10 @@
 package mini_project.server.repository;
 
 import org.springframework.beans.factory.annotation.Value;
-import org.springframework.http.RequestEntity;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Repository;
 import org.springframework.web.client.RestTemplate;
 import org.springframework.web.util.UriComponentsBuilder;
-
 
 @Repository
 public class GoogleRepository {
@@ -15,23 +13,16 @@ public class GoogleRepository {
     private String apiKey;
 
     public ResponseEntity<String> getGeocode(String address) {
-
-        System.out.println(address);
-        System.out.println(address.toLowerCase());
-
         String url = UriComponentsBuilder
             .fromUriString("https://maps.googleapis.com/maps/api/geocode/json")
             .queryParam("address", address.toLowerCase())
             .queryParam("key", apiKey)
             .toUriString();
-
         System.out.println(url);
 
-        RequestEntity<Void> req = RequestEntity.get(url).build();
         RestTemplate template = new RestTemplate();
-
         System.out.println(template.getForEntity(url, String.class));
         return template.getForEntity(url, String.class);
-        // return template.exchange(req, String.class);
     }    
 }
+
