f821042dfe10295ff0b4ea30d01be7d1f81cbb80
LydiaThamy <lydiathamy@gmail.com>
2023-09-09 17:26:49 +0800

    add dependency and POJO class for spring security

---
+++ server/src/main/java/mini_project/server/model/User.java
@@ -0,0 +1,13 @@
+package mini_project.server.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class User {
+    String username;
+    String password;
+}
