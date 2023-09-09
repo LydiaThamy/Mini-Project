24cb354d8aa007278542c32777941064c5fd5671
LydiaThamy <lydiathamy@gmail.com>
2023-09-09 21:36:24 +0800

    add spring security

---
+++ server/src/main/java/mini_project/server/repository/UserRepository.java
@@ -0,0 +1,22 @@
+package mini_project.server.repository;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.stereotype.Repository;
+
+import mini_project.server.model.User;
+
+@Repository
+public class UserRepository {
+
+    @Autowired
+    private JdbcTemplate template;
+
+    public User getUserById(Integer id) {
+        return null;
+    }
+
+    public User getUserByUsername(String username) {
+        return null;
+    }
+}
