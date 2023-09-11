93ddcd80063bae87cc12be5a648fc51796cd3b43
LydiaThamy <lydiathamy@gmail.com>
2023-09-11 15:40:08 +0800

    remove unused code

--- server/src/main/java/mini_project/server/repository/SqlRepository.java
+++ server/src/main/java/mini_project/server/repository/SqlRepository.java
@@ -2,7 +2,6 @@ package mini_project.server.repository;
 
 import java.util.LinkedList;
 import java.util.List;
-import java.util.Map;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
--- server/src/main/java/mini_project/server/service/ShophouseService.java
+++ server/src/main/java/mini_project/server/service/ShophouseService.java
@@ -2,7 +2,6 @@ package mini_project.server.service;
 
 import java.io.IOException;
 import java.util.List;
-import java.util.Map;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
