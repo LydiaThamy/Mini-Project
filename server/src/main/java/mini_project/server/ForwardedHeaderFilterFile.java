0872d06532009515da4f6f7cf1c64ad178593f7c
LydiaThamy <lydiathamy@gmail.com>
2023-09-14 00:32:43 +0800

    forward https request to http

---
+++ server/src/main/java/mini_project/server/ForwardedHeaderFilterFile.java
@@ -0,0 +1,23 @@
+package mini_project.server;
+
+// import java.util.List;
+
+import org.springframework.boot.web.servlet.FilterRegistrationBean;
+import org.springframework.context.annotation.Bean;
+import org.springframework.core.Ordered;
+import org.springframework.web.filter.ForwardedHeaderFilter;
+
+import jakarta.servlet.DispatcherType;
+
+public class ForwardedHeaderFilterFile {
+    
+    @Bean
+    public FilterRegistrationBean<ForwardedHeaderFilter> forwardedHeaderFilter() {
+        ForwardedHeaderFilter filter = new ForwardedHeaderFilter();
+        FilterRegistrationBean<ForwardedHeaderFilter> registration = new FilterRegistrationBean<>(filter);
+        registration.setDispatcherTypes(DispatcherType.REQUEST, DispatcherType.ASYNC, DispatcherType.ERROR);
+        registration.setOrder(Ordered.HIGHEST_PRECEDENCE);
+        // registration.setUrlPatterns(List.of("/absoluteURLWithFilter"));
+        return registration;
+    }
+}
