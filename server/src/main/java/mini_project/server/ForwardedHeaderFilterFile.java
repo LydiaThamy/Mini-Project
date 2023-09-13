b646293c8ab6703b70b8f2fb7b735a367d7d502e
LydiaThamy <lydiathamy@gmail.com>
2023-09-14 00:48:29 +0800

    place forwarded header filter in configuration bean

--- server/src/main/java/mini_project/server/ForwardedHeaderFilterFile.java
+++ server/src/main/java/mini_project/server/ForwardedHeaderFilterFile.java
@@ -4,11 +4,13 @@ package mini_project.server;
 
 import org.springframework.boot.web.servlet.FilterRegistrationBean;
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
 import org.springframework.core.Ordered;
 import org.springframework.web.filter.ForwardedHeaderFilter;
 
 import jakarta.servlet.DispatcherType;
 
+@Configuration
 public class ForwardedHeaderFilterFile {
     
     @Bean
