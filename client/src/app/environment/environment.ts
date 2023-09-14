de76307df55fa20e5436a4f9b9a0cab2bd1c9420
LydiaThamy <lydiathamy@gmail.com>
2023-09-14 12:22:45 +0800

    debug spring security config and oauth routing

--- client/src/app/environment/environment.ts
+++ client/src/app/environment/environment.ts
@@ -3,5 +3,5 @@ export const environment = {
     stripe: 'pk_test_51NoOROLLMJMhSmwKcS6RV7wJucbExXpDSHl1gvQDMQBuwF8m8e0pAvJ2DI9imQh9Qw8cLR0DWRf52CuKTy02z7Jw00aD3BT12B',
     github: 'c8885ae2376e95046357',
     // baseUrl: 'https://cors-server.up.railway.app'
-    baseUrl: 'http://localhost:8080'
+    baseUrl: 'http://localhost:4200'
 }
